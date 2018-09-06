b26d10486a9662b58f8f285fcc95b95b0767d6ae
Nam Nguyen <nam.quoine@gmail.com>
2018-09-06 13:57:06 +0700

    Init

---
+++ rollup.config.js
@@ -0,0 +1,7 @@
+export default {
+    input: 'src/index.js',
+    output: {
+        file: 'rollup/build.js',
+        format: 'cjs'
+    }
+};
---
+++ rollup/build.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var index = "^_^";
+
+module.exports = index;
